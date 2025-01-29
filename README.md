6a1edd7de9eadf6e0edd9bd12356078ae5cdbd27
dhananjoycd <dcdmf2020@gmail.com>
2025-01-30 01:17:53 +0600

    Primary setup done

---
+++ README.md
@@ -0,0 +1,6 @@
+## In this project I have used:
+
+- vite for react
+- tailwind 4
+- daisyUi 5
+- react router dom
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-red-400">Mission Start</p>
+    </>
+  );
+}
+
+export default App;
